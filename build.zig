8c74625df894885a8258e50c16f8af992f1c5d89
Scotte Zinn <scotte@zinn.ca>
2025-12-03 04:31:22 -0500

    Initial template

---
+++ build.zig
@@ -0,0 +1,116 @@
+const std = @import("std");
+const Build = std.Build;
+const CompileStep = std.Build.Step.Compile;
+
+/// set this to true to link libc
+const should_link_libc = false;
+
+fn linkObject(b: *Build, obj: *CompileStep) void {
+    if (should_link_libc) obj.linkLibC();
+    _ = b;
+
+    // Add linking for packages or third party libraries here
+}
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const generate = b.step("generate", "Generate stub files from template/template.zig");
+    const build_generate = b.addExecutable(.{
+        .name = "generate",
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("template/generate.zig"),
+            .target = target,
+            .optimize = optimize,
+        }),
+    });
+
+    const run_generate = b.addRunArtifact(build_generate);
+    run_generate.setCwd(b.path("")); // This could probably be done in a more idiomatic way
+    generate.dependOn(&run_generate.step);
+
+    const install_all = b.step("install_all", "Install all days");
+    const run_all = b.step("run_all", "Run all days");
+
+    // Set up an exe for each day
+    var day: u32 = 1;
+    while (day <= 25) : (day += 1) {
+        const dayString = b.fmt("day{:0>2}", .{day});
+        const zigFile = b.fmt("src/{s}.zig", .{dayString});
+
+        const exe = b.addExecutable(.{
+            .name = dayString,
+            .root_module = b.createModule(.{
+                .root_source_file = b.path(zigFile),
+                .target = target,
+                .optimize = optimize,
+            }),
+        });
+        linkObject(b, exe);
+
+        const install_cmd = b.addInstallArtifact(exe, .{});
+
+        const build_test = b.addTest(.{
+            .root_module = b.createModule(.{
+                .root_source_file = b.path(zigFile),
+                .target = target,
+                .optimize = optimize,
+            }),
+        });
+        linkObject(b, build_test);
+
+        const run_test = b.addRunArtifact(build_test);
+
+        {
+            const step_key = b.fmt("install_{s}", .{dayString});
+            const step_desc = b.fmt("Install {s}.exe", .{dayString});
+            const install_step = b.step(step_key, step_desc);
+            install_step.dependOn(&install_cmd.step);
+            install_all.dependOn(&install_cmd.step);
+        }
+
+        {
+            const step_key = b.fmt("test_{s}", .{dayString});
+            const step_desc = b.fmt("Run tests in {s}", .{zigFile});
+            const step = b.step(step_key, step_desc);
+            step.dependOn(&run_test.step);
+        }
+
+        const run_cmd = b.addRunArtifact(exe);
+        if (b.args) |args| {
+            run_cmd.addArgs(args);
+        }
+
+        const run_desc = b.fmt("Run {s}", .{dayString});
+        const run_step = b.step(dayString, run_desc);
+        run_step.dependOn(&run_cmd.step);
+        run_all.dependOn(&run_cmd.step);
+    }
+
+    // Set up tests for util.zig
+    {
+        const test_util = b.step("test_util", "Run tests in util.zig");
+        const test_cmd = b.addTest(.{
+            .root_module = b.createModule(.{
+                .root_source_file = b.path("src/util.zig"),
+                .target = target,
+                .optimize = optimize,
+            }),
+        });
+        linkObject(b, test_cmd);
+        test_util.dependOn(&test_cmd.step);
+    }
+
+    // Set up all tests contained in test_all.zig
+    const test_all = b.step("test", "Run all tests");
+    const all_tests = b.addTest(.{
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("src/test_all.zig"),
+            .target = target,
+            .optimize = optimize,
+        }),
+    });
+    const run_all_tests = b.addRunArtifact(all_tests);
+    test_all.dependOn(&run_all_tests.step);
+}
