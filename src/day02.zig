e544512bcc1f5c6a3d577469ce843e24a3eb1950
Scotte Zinn <scotte@zinn.ca>
2025-12-04 07:39:03 -0500

    Day 2 complete

--- src/day02.zig
+++ src/day02.zig
@@ -9,6 +9,7 @@ const util = @import("util.zig");
 const gpa = util.gpa;
 
 const data = @embedFile("data/day02.txt");
+const data_sample = "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124";
 
 // Useful stdlib functions
 const tokenizeAny = std.mem.tokenizeAny;
@@ -40,8 +41,93 @@ const desc = std.sort.desc;
 const expect = std.testing.expect;
 const expectEqual = std.testing.expectEqual;
 
-pub fn main() !void {}
+pub fn main() !void {
+    const result = try day02(data);
+    print("Answer is {}\n", .{result});
+}
 
-// Generated from template/template.zig.
-// Run `zig build generate` to update.
-// Only unmodified days will be updated.
+fn day02(input: []const u8) !struct { stage1: usize, stage2: usize } {
+    var rangeIterator = tokenizeSeq(u8, input, ",");
+
+    var sum_stage1: usize = 0;
+    var sum_stage2: usize = 0;
+    while (rangeIterator.next()) |range| {
+        const trimmedRange = trim(u8, range, "\n");
+        if (indexOf(u8, trimmedRange, '-')) |index| {
+            const lower = try parseInt(usize, trimmedRange[0..index], 10);
+            const upper = try parseInt(usize, trimmedRange[index + 1 ..], 10);
+
+            sum_stage1 += sumInvalidStageIds(lower, upper, isValidStage1Id);
+            sum_stage2 += sumInvalidStageIds(lower, upper, isValidStage2Id);
+        }
+    }
+
+    return .{ .stage1 = sum_stage1, .stage2 = sum_stage2 };
+}
+
+fn sumInvalidStageIds(lower: usize, upper: usize, comptime isValidId: fn (usize) bool) usize {
+    var sum: usize = 0;
+
+    for (lower..upper + 1) |i| {
+        if (!isValidId(i)) {
+            sum += i;
+        }
+    }
+    return sum;
+}
+
+fn isValidStage1Id(value: usize) bool {
+    const len = std.math.log10_int(value) + 1;
+
+    if (len & 1 == 1) {
+        return true;
+    }
+
+    const mask = std.math.powi(usize, 10, len / 2) catch {
+        unreachable;
+    };
+
+    return (value / mask) != @mod(value, mask);
+}
+
+fn isValidStage2Id(value: usize) bool {
+    const len = std.math.log10_int(value) + 1;
+    for (1..(len / 2 + 1)) |digitCount| {
+        if (@mod(len - digitCount, digitCount) != 0) continue;
+
+        const mask: usize = std.math.powi(usize, 10, digitCount) catch {
+            unreachable;
+        };
+        const digits = @mod(value, mask);
+        var sum: usize = 0;
+        for (0..(len / digitCount)) |x| {
+            const shift: usize = std.math.powi(usize, 10, digitCount * x) catch {
+                unreachable;
+            };
+            sum += digits * shift;
+        }
+        if (sum == value) return false;
+    }
+
+    return true;
+}
+
+test "example data" {
+    const result = try day02(data_sample);
+
+    try expectEqual(1227775554, result.stage1);
+}
+
+test "check valid stage1" {
+    try expect(!isValidStage1Id(11));
+    try expect(isValidStage1Id(111));
+    try expect(isValidStage1Id(12));
+    try expect(!isValidStage1Id(1212));
+}
+
+test "check valid stage2" {
+    try expect(!isValidStage2Id(11));
+    try expect(!isValidStage2Id(12341234));
+    try expect(isValidStage2Id(123));
+    try expect(!isValidStage2Id(12121212));
+}
