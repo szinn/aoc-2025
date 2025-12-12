c752806b3dcdd7e174662ab71fae4da67d91b5b8
Scotte Zinn <scotte@zinn.ca>
2025-12-12 07:16:59 -0500

    Day 6 complete

--- src/day06.zig
+++ src/day06.zig
@@ -9,6 +9,12 @@ const util = @import("util.zig");
 const gpa = util.gpa;
 
 const data = @embedFile("data/day06.txt");
+const dataSample =
+    \\123 328  51 64 
+    \\ 45 64  387 23 
+    \\  6 98  215 314
+    \\*   +   *   +  
+;
 
 // Useful stdlib functions
 const tokenizeAny = std.mem.tokenizeAny;
@@ -40,8 +46,113 @@ const desc = std.sort.desc;
 const expect = std.testing.expect;
 const expectEqual = std.testing.expectEqual;
 
-pub fn main() !void {}
+const Operation = enum {
+    UNKNOWN,
+    MULTIPLY,
+    ADD,
+};
 
-// Generated from template/template.zig.
-// Run `zig build generate` to update.
-// Only unmodified days will be updated.
+const Operator = struct {
+    operation: Operation,
+    column: usize,
+};
+
+pub fn main() !void {
+    const results = try day06(data);
+
+    print("Answer: {}\n", .{results});
+}
+
+fn day06(input: []const u8) !struct { usize, usize } {
+    var lines: List([]const u8) = .init(gpa);
+    var operators: List(Operator) = .init(gpa);
+    defer {
+        lines.deinit();
+        operators.deinit();
+    }
+
+    var lineIterator = tokenizeSeq(u8, input, "\n");
+    while (lineIterator.next()) |line| {
+        if (line.len == 0) continue;
+
+        switch (line[0]) {
+            '*', '+' => {
+                for (line, 0..) |c, i| {
+                    switch (c) {
+                        '*' => {
+                            try operators.append(Operator{ .operation = Operation.MULTIPLY, .column = i });
+                        },
+                        '+' => {
+                            try operators.append(Operator{ .operation = Operation.ADD, .column = i });
+                        },
+                        else => {},
+                    }
+                }
+                try operators.append(Operator{ .operation = Operation.UNKNOWN, .column = line.len });
+            },
+
+            else => {
+                try lines.append(line);
+            },
+        }
+    }
+
+    var sumStage1: usize = 0;
+    var sumStage2: usize = 0;
+    const operatorItems = operators.items;
+    for (0..operatorItems.len - 1) |i| {
+        const operator = operatorItems[i];
+        const startColumn = operator.column;
+        const endColumn = operatorItems[i + 1].column;
+
+        // stage1
+        var resultStage1: usize = identity(operator.operation);
+        for (lines.items) |line| {
+            const numberText = std.mem.trim(u8, line[operator.column..endColumn], " ");
+            const number = try parseInt(usize, numberText, 10);
+            switch (operator.operation) {
+                Operation.ADD => resultStage1 += number,
+                Operation.MULTIPLY => resultStage1 *= number,
+                else => {},
+            }
+        }
+        sumStage1 += resultStage1;
+
+        // stage2
+        var result: usize = identity(operator.operation);
+        for (startColumn..endColumn) |column| {
+            var number: usize = 0;
+            var hasDigit: bool = false;
+            for (lines.items) |line| {
+                const c: u8 = line[column];
+
+                if (c != ' ') {
+                    hasDigit = true;
+                    number = number * 10 + (c - '0');
+                }
+            }
+            if (hasDigit) {
+                switch (operator.operation) {
+                    Operation.ADD => result += number,
+                    Operation.MULTIPLY => result *= number,
+                    else => {},
+                }
+            }
+        }
+        sumStage2 += result;
+    }
+
+    return .{ sumStage1, sumStage2 };
+}
+
+fn identity(operation: Operation) usize {
+    return switch (operation) {
+        .ADD => 0,
+        .MULTIPLY => 1,
+        else => 0,
+    };
+}
+
+test "Sample data" {
+    try expectEqual(.{ 4277556, 3263827 }, day06(dataSample));
+}
