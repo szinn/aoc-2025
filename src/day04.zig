d88e81121513631c575542133ca6417a2e12c485
Scotte Zinn <scotte@zinn.ca>
2026-01-04 14:50:05 -0500

    Day 4 solution

--- src/day04.zig
+++ src/day04.zig
@@ -1,13 +1,47 @@
 const std = @import("std");
-
 const Allocator = std.mem.Allocator;
-const HashMap = std.array_hash_map.AutoArrayHashMap;
 const List = std.array_list.Managed;
+const Map = std.AutoHashMap;
+const StrMap = std.StringHashMap;
+const BitSet = std.DynamicBitSet;
+
+const data = @embedFile("data/day04.txt");
+const sampleData =
+    \\..@@.@@@@.
+    \\@@@.@.@.@@
+    \\@@@@@.@.@@
+    \\@.@@@@..@.
+    \\@@.@@@@.@@
+    \\.@@@@@@@.@
+    \\.@.@.@.@@@
+    \\@.@@@.@@@@
+    \\.@@@@@@@@.
+    \\@.@.@@@.@.
+;
+
+// Useful stdlib functions
+const tokenizeAny = std.mem.tokenizeAny;
+const tokenizeSeq = std.mem.tokenizeSequence;
+const tokenizeSca = std.mem.tokenizeScalar;
+const splitAny = std.mem.splitAny;
+const splitSeq = std.mem.splitSequence;
+const splitSca = std.mem.splitScalar;
+const indexOf = std.mem.indexOfScalar;
+const indexOfAny = std.mem.indexOfAny;
+const indexOfStr = std.mem.indexOfPosLinear;
+const lastIndexOf = std.mem.lastIndexOfScalar;
+const lastIndexOfAny = std.mem.lastIndexOfAny;
+const lastIndexOfStr = std.mem.lastIndexOfLinear;
+const trim = std.mem.trim;
+const sliceMin = std.mem.min;
+const sliceMax = std.mem.max;
+
+const parseInt = std.fmt.parseInt;
+const parseFloat = std.fmt.parseFloat;
 
 const print = std.debug.print;
 const assert = std.debug.assert;
-const splitScalar = std.mem.splitScalar;
-const parseInt = std.fmt.parseInt;
+
 const sort = std.sort.block;
 const asc = std.sort.asc;
 const desc = std.sort.desc;
@@ -15,34 +49,115 @@ const desc = std.sort.desc;
 const expect = std.testing.expect;
 const expectEqual = std.testing.expectEqual;
 
-const data = @embedFile("data/day04.txt");
-const sampleData =
-    \\
-;
+const Cell = enum(u8) { OOB = 'x', EMPTY = '.', ROLL = '@' };
+const Grid = List([]Cell);
 
-// pub fn main() !void {
-//     var debugAllocator: std.heap.DebugAllocator(.{}) = .init;
-//     defer assert(debugAllocator.deinit() == .ok);
-//
-//     const result = try day04(debugAllocator.allocator(), data);
-//
-//     print("Result = {}\n", .{result});
-// }
-//
-// fn day04(allocator: Allocator, input: []const u8) !struct {} {
-//     var arenaAllocator: std.heap.ArenaAllocator = .init(allocator);
-//     defer arenaAllocator.deinit();
-//     const arena = arenaAllocator.allocator();
-//
-//     return .{};
-// }
-//
-// test "Sample data" {
-//     const allocator = std.testing.allocator;
-//
-//     try expectEqual(.{}, try day04(allocator, sampleData));
-// }
+pub fn main() !void {
+    var debugAllocator: std.heap.DebugAllocator(.{}) = .init;
+    defer assert(debugAllocator.deinit() == .ok);
 
-// Generated from template/template.zig.
-// Run `zig build generate` to update.
-// Only unmodified days will be updated.
+    const results = try day04(debugAllocator.allocator(), data);
+
+    print("Answer = {}", .{results});
+}
+
+fn day04(allocator: Allocator, input: []const u8) !struct { usize, usize } {
+    var arenaAllocator: std.heap.ArenaAllocator = .init(allocator);
+    defer arenaAllocator.deinit();
+    const arena = arenaAllocator.allocator();
+
+    var grid: Grid = .init(arena);
+    defer {
+        for (grid.items) |row| arena.free(row);
+        grid.deinit();
+    }
+
+    try loadGrid(arena, input, &grid);
+
+    var removeable: List([2]i32) = .init(arena);
+    defer {
+        removeable.deinit();
+    }
+
+    try getRemoveable(&grid, &removeable);
+    const stage1 = removeable.items.len;
+
+    var stage2: usize = 0;
+
+    while (removeable.items.len > 0) {
+        while (removeable.pop()) |cell| {
+            const row, const column = cell;
+            grid.items[@intCast(row)][@intCast(column)] = Cell.EMPTY;
+            stage2 += 1;
+        }
+        try getRemoveable(&grid, &removeable);
+    }
+
+    return .{ stage1, stage2 };
+}
+
+fn getRemoveable(grid: *Grid, removeable: *List([2]i32)) !void {
+    for (0..grid.items.len) |row| {
+        for (0..grid.items[row].len) |column| {
+            if (isRemovable(grid, @intCast(row), @intCast(column))) {
+                try removeable.append(.{ @intCast(row), @intCast(column) });
+            }
+        }
+    }
+}
+
+fn loadGrid(allocator: Allocator, input: []const u8, grid: *Grid) !void {
+    var lineIterator = tokenizeSeq(u8, input, "\n");
+    while (lineIterator.next()) |line| {
+        if (line.len == 0) continue;
+
+        const row = try allocator.alloc(Cell, line.len);
+        std.mem.copyForwards(Cell, row, @ptrCast(line));
+        try grid.append(row);
+    }
+}
+
+fn isRemovable(grid: *Grid, row: i32, column: i32) bool {
+    if (getContents(grid, row, column) != Cell.ROLL) return false;
+
+    var rolls: usize = 0;
+    for (neighbours(row, column)) |rc| {
+        if (getContents(grid, rc[0], rc[1]) == Cell.ROLL) {
+            rolls += 1;
+        }
+    }
+
+    return rolls < 4;
+}
+
+fn getContents(grid: *Grid, row: i32, column: i32) Cell {
+    if (row < 0 or row >= grid.items.len) return Cell.OOB;
+    if (column < 0 or column >= grid.items[0].len) return Cell.OOB;
+
+    return grid.items[@intCast(row)][@intCast(column)];
+}
+
+fn neighbours(row: i32, column: i32) [8]struct { i32, i32 } {
+    return .{
+        .{ row - 1, column - 1 },
+        .{ row - 1, column },
+        .{ row - 1, column + 1 },
+        .{ row, column - 1 },
+        .{ row, column + 1 },
+        .{ row + 1, column - 1 },
+        .{ row + 1, column },
+        .{ row + 1, column + 1 },
+    };
+}
+
+test "Sample data" {
+    const allocator = std.testing.allocator;
+
+    try expectEqual(.{ 13, 43 }, try day04(allocator, sampleData));
+}
+
+test "real data" {
+    const allocator = std.testing.allocator;
+
+    try expectEqual(.{ 1553, 8442 }, try day04(allocator, data));
+}
