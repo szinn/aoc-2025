9e2b8524d7de43e866767c4cf94c7852cdeb38cc
Scotte Zinn <scotte@zinn.ca>
2026-01-04 14:27:31 -0500

    Initial template creation

---
+++ src/test_all.zig
@@ -0,0 +1,15 @@
+test {
+    _ = @import("day01.zig");
+    _ = @import("day02.zig");
+    _ = @import("day03.zig");
+    _ = @import("day04.zig");
+    _ = @import("day05.zig");
+    _ = @import("day06.zig");
+    _ = @import("day07.zig");
+    _ = @import("day08.zig");
+    _ = @import("day09.zig");
+    _ = @import("day10.zig");
+    _ = @import("day11.zig");
+    _ = @import("day12.zig");
+    // Import any other files with tests here
+}
